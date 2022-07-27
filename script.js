4f4ebe28f371993cf408cbf7836ca7443f3c06bb
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-07-27 20:15:10 -0300

    theme change bug fixed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // troca de tema
 
 const chk = document.getElementById("chk");
-const theme = document.querySelectorAll(".theme");
+let theme = document.querySelectorAll(".theme");
 
 chk.addEventListener("click", () => {
   document.body.classList.toggle("dark");
@@ -11,7 +11,7 @@ chk.addEventListener("click", () => {
 // funcionalidades 
 
 const input = document.getElementById("to_do_input");
-const tarefas = document.querySelectorAll(".item");
+let tarefas = document.querySelectorAll(".item");
 const lista = document.querySelector(".lista");
 
 function adicionarTarefa() {
@@ -21,20 +21,23 @@ function adicionarTarefa() {
   let deletar = document.createElement("div");
   let novaTarefa = document.createTextNode(input.value);
 
-  novoItem.classList.add("item")
-  novoItem.classList.add("theme")
-  circulo.classList.add("circulo")
-  texto.classList.add("item_texto")
-  texto.classList.add("theme")
-  deletar.classList.add("deletar_item")
+  novoItem.classList.add("item");
+  novoItem.classList.add("theme");
+  circulo.classList.add("circulo");
+  texto.classList.add("item_texto");
+  texto.classList.add("theme");
+  deletar.classList.add("deletar_item");
 
-  texto.appendChild(novaTarefa)
-  novoItem.appendChild(circulo) // div circulo
+  texto.appendChild(novaTarefa);
+  novoItem.appendChild(circulo);
   novoItem.appendChild(texto);
-  novoItem.appendChild(deletar) // div deletar item
+  novoItem.appendChild(deletar); 
 
   lista.appendChild(novoItem);
+
   input.value = "";
+  tarefas = document.querySelectorAll(".item");
+  theme = document.querySelectorAll(".theme");
 }
 
 /* ADICIONAR TAREFA AO PRESSIONAR 'ENTER' */
