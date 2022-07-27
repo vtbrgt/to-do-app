c5be25f398d21e7048bcc0f6d92cfc2d2d60c912
Vitor Borgatte <79037838+vtbrgt@users.noreply.github.com>
2022-07-27 20:10:11 -0300

    add new task is now functional

--- script.js
+++ script.js
@@ -1,51 +1,47 @@
+// troca de tema
+
 const chk = document.getElementById("chk");
 const theme = document.querySelectorAll(".theme");
-const clear = document.querySelector(".clear");
-const input = document.getElementById("to_do_input");
-/* const lista = document.getElementsByClassName("lista"); */
 
-var ul = document.getElementById("ul");
-var item = document.getElementsByTagName("li");
-
-function inputLength() {
-  return input.value.length;
-}
-
-function adicionarTarefa() {
-  var li = document.createElement("li");
-
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
-  input.value = "";
-
-  function tarefaConcluida() {
-    li.classList.toggle("done");
-  }
-
-  li.addEventListener("click", tarefaConcluida);
-
-  let botaoDeletar = document.createElement("button");
-  botaoDeletar.appendChild(document.createTextNode("X"));
-  li.appendChild(botaoDeletar);
-  botaoDeletar.addEventListener("click", deletarItem);
-
-  function deletarItem() {
-    li.classList.add("item");
-    li.classList.add("theme");
-    li.classList.add("delete");
-  }
-}
-
-input.addEventListener("keypress", addListaAfterKeypress);
-
-function addListaAfterKeypress() {
-  if (inputLength() > 0 && event.which === 13) {
-    adicionarTarefa();
-  }
-}
-
-/* FUNÇÃO PARA MUDAR O TEMA */
 chk.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   theme.forEach((each) => each.classList.toggle("dark"));
 });
+
+// funcionalidades 
+
+const input = document.getElementById("to_do_input");
+const tarefas = document.querySelectorAll(".item");
+const lista = document.querySelector(".lista");
+
+function adicionarTarefa() {
+  let novoItem = document.createElement("li");
+  let circulo = document.createElement("div");
+  let texto = document.createElement("p");
+  let deletar = document.createElement("div");
+  let novaTarefa = document.createTextNode(input.value);
+
+  novoItem.classList.add("item")
+  novoItem.classList.add("theme")
+  circulo.classList.add("circulo")
+  texto.classList.add("item_texto")
+  texto.classList.add("theme")
+  deletar.classList.add("deletar_item")
+
+  texto.appendChild(novaTarefa)
+  novoItem.appendChild(circulo) // div circulo
+  novoItem.appendChild(texto);
+  novoItem.appendChild(deletar) // div deletar item
+
+  lista.appendChild(novoItem);
+  input.value = "";
+}
+
+/* ADICIONAR TAREFA AO PRESSIONAR 'ENTER' */
+function addListaAfterKeypress() {
+  if (event.which === 13) {
+    adicionarTarefa();
+  }
+}
+
+input.addEventListener("keypress", addListaAfterKeypress);
