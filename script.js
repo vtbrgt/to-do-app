6897e68e831bd807799c804f8db2e01e7f8f524e
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-01 10:51:29 -0300

    deleting tasks is now functional

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const input = document.getElementById('to_do_input');
 let tarefas = Array.from(document.querySelectorAll('.item'));
 const lista = document.querySelector('.lista');
-let quantidadeTarefas = tarefas.length;
 let circulos = Array.from(document.querySelectorAll('.circulo'));
+let deletes = Array.from(document.querySelectorAll('.deletar_item'));
 
 const chk = document.getElementById('chk');
 let theme = document.querySelectorAll('.theme');
@@ -16,8 +16,10 @@ chk.addEventListener('click', () => {
 // QUANTIDADE DE TAREFAS NA LISTA
 function atualizaQuantidade() {
   const span = document.querySelector('.quantidade_tarefas');
+  let quantidadeTarefas = tarefas.length;
 
   span.innerText = quantidadeTarefas;
+  console.log(quantidadeTarefas);
 }
 
 document.addEventListener('load', atualizaQuantidade());
@@ -55,10 +57,12 @@ function adicionarTarefa() {
   input.value = '';
   tarefas.push(novoItem);
   circulos.push(circulo);
+  deletes.push(deletar);
   theme = document.querySelectorAll('.theme');
   quantidadeTarefas = tarefas.length;
   atualizaQuantidade();
   tarefaConcluida();
+  deletarTarefa();
 }
 
 // ADICIONAR TAREFA AO PRESSIONAR 'ENTER'
@@ -74,10 +78,32 @@ input.addEventListener('keypress', addListaAfterKeypress);
 function tarefaConcluida() {
   circulos.forEach((circulo) => {
     circulo.addEventListener('click', (event) => {
-      let i = circulos.indexOf(event.target);
+      const i = circulos.indexOf(event.target);
       circulos[i].classList.toggle('done');
       tarefas[i].classList.toggle('done');
     });
   });
 }
 tarefaConcluida();
+
+function deletarTarefa() {
+  deletes.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      const i = deletes.indexOf(event.target);
+      event.target.parentElement.remove();
+      tarefas.splice(i, 1);
+      atualizaQuantidade();
+    });
+  });
+}
+deletarTarefa();
+
+// DELETAR TAREFA
+/* function deletarTarefa(e) {
+  if (e.target.className === 'deletar_item') {
+    e.target.parentElement.remove();
+  }
+  atualizaQuantidade();
+}
+
+lista.addEventListener('click', deletarTarefa); */
