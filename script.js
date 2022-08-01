0da65ad815f7d2fb861bc0b50a5f1270262d0888
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-08-01 00:22:07 -0300

    fixing bugs

--- script.js
+++ script.js
@@ -1,84 +1,83 @@
-const input = document.getElementById("to_do_input");
-let tarefas = Array.from(document.querySelectorAll(".item"));
-const lista = document.querySelector(".lista");
+const input = document.getElementById('to_do_input');
+let tarefas = Array.from(document.querySelectorAll('.item'));
+const lista = document.querySelector('.lista');
 let quantidadeTarefas = tarefas.length;
 let circulos = Array.from(document.querySelectorAll('.circulo'));
 
-const chk = document.getElementById("chk");
-let theme = document.querySelectorAll(".theme");
+const chk = document.getElementById('chk');
+let theme = document.querySelectorAll('.theme');
 
 // TROCA DE TEMA
-chk.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  theme.forEach((each) => each.classList.toggle("dark"));
+chk.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  theme.forEach((each) => each.classList.toggle('dark'));
 });
 
-// QUANTIDADE DE TAREFAS NA LISTA 
+// QUANTIDADE DE TAREFAS NA LISTA
 function atualizaQuantidade() {
-  const span = document.querySelector(".quantidade_tarefas");
+  const span = document.querySelector('.quantidade_tarefas');
 
   span.innerText = quantidadeTarefas;
 }
 
 document.addEventListener('load', atualizaQuantidade());
 
-// FUNCIONALIDADES 
+// FUNCIONALIDADES
 function adicionarTarefa() {
-  let novoItem = document.createElement("li");
-  let circulo = document.createElement("div");
-  let texto = document.createElement("p");
-  let deletar = document.createElement("div");
+  let novoItem = document.createElement('li');
+  let circulo = document.createElement('div');
+  let texto = document.createElement('p');
+  let deletar = document.createElement('div');
   let novaTarefa = document.createTextNode(input.value);
 
-  novoItem.classList.add("item");
-  circulo.classList.add("circulo");
-  texto.classList.add("item_texto");
-  deletar.classList.add("deletar_item");
+  novoItem.classList.add('item');
+  circulo.classList.add('circulo');
+  texto.classList.add('item_texto');
+  deletar.classList.add('deletar_item');
 
-  if (document.body.classList.contains("dark")) {
-    novoItem.classList.add("theme");
-    novoItem.classList.add("dark");
-    texto.classList.add("theme");
-    texto.classList.add("dark");
+  if (document.body.classList.contains('dark')) {
+    novoItem.classList.add('theme');
+    novoItem.classList.add('dark');
+    texto.classList.add('theme');
+    texto.classList.add('dark');
   } else {
-    novoItem.classList.add("theme");
-    texto.classList.add("theme");
+    novoItem.classList.add('theme');
+    texto.classList.add('theme');
   }
-  
 
   texto.appendChild(novaTarefa);
   novoItem.appendChild(circulo);
   novoItem.appendChild(texto);
-  novoItem.appendChild(deletar); 
+  novoItem.appendChild(deletar);
 
   lista.appendChild(novoItem);
 
-  input.value = "";
+  input.value = '';
   tarefas.push(novoItem);
   circulos.push(circulo);
-  theme = document.querySelectorAll(".theme");
+  theme = document.querySelectorAll('.theme');
   quantidadeTarefas = tarefas.length;
   atualizaQuantidade();
+  tarefaConcluida();
 }
 
-
-
-  // ADICIONAR TAREFA AO PRESSIONAR 'ENTER'
-function addListaAfterKeypress() {
-  if (event.which === 13) {
+// ADICIONAR TAREFA AO PRESSIONAR 'ENTER'
+function addListaAfterKeypress(tecla) {
+  if (tecla.key === 'Enter' && input.value != '') {
     adicionarTarefa();
   }
 }
 
-input.addEventListener("keypress", addListaAfterKeypress);
+input.addEventListener('keypress', addListaAfterKeypress);
 
-  // MARCAR TAREFA COMO CONCLUÍDA
-circulos.forEach((circulo) => {
-  circulo.addEventListener('click', (event) => {
-    let i = circulos.indexOf(event.target);
-    console.log(i)
-
-    circulos[i].classList.toggle('done');
-    tarefas[i].classList.toggle('done');
-  })
-})
+// MARCAR TAREFA COMO CONCLUÍDA
+function tarefaConcluida() {
+  circulos.forEach((circulo) => {
+    circulo.addEventListener('click', (event) => {
+      let i = circulos.indexOf(event.target);
+      circulos[i].classList.toggle('done');
+      tarefas[i].classList.toggle('done');
+    });
+  });
+}
+tarefaConcluida();
